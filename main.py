4e1a167af6de39316116ef77b117dada5ac122f9
Tanuj Sood <80703278+Tanuj-Sood@users.noreply.github.com>
2024-06-20 17:35:53 +0530

    Update main.py
    hashed out some lines of code due to a bug that was preventing correct environment variables from being used in the transcribe() function

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ trim = Trim()
 
 def run():
     #code for getting user inputted filename
-    filename= str(input("Enter File name:-"))
-    dotenv_file = dotenv.find_dotenv()
-    dotenv.load_dotenv(dotenv_file)
+    #filename= str(input("Enter File name:-"))
+    #dotenv_file = dotenv.find_dotenv()
+    #dotenv.load_dotenv(dotenv_file)
 
-    os.environ["BASE_VIDEO"] = filename
+    #os.environ["BASE_VIDEO"] = filename
     # Write changes to .env file.
-    dotenv.set_key(dotenv_file, "BASE_VIDEO", os.environ["BASE_VIDEO"])
+    #dotenv.set_key(dotenv_file, "BASE_VIDEO", os.environ["BASE_VIDEO"])
 
     #calling of the functions
     transcription.transcribe()
